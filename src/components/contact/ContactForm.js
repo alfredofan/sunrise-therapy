7db221851b9e588e42a0df2a0e9a280aa87ac13c
AlfredoFAN <fan.alfredo@gmail.com>
2024-04-21 12:59:47 +1000

    summit and contact form fix

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -61,7 +61,7 @@ const ContactForm = () => {
       setIsSubmitting(true);
       try {
         const response = await axios.post(
-          '/.netlify/functions/submit',
+          '/.netlify/functions/submit-form',
           {
             name: formData.name,
             email: formData.email,
